93a2740225a3025ee3c78a6a9893a596ee24aaf8
lukasz5550 <lukasz5550@poczta.onet.pl>
2018-04-07 16:26:16 +0200

    Commit4

--- app/src/main/java/com/ldmitrowski/wizut/appandroidlab/MainActivity.java
+++ app/src/main/java/com/ldmitrowski/wizut/appandroidlab/MainActivity.java
@@ -46,4 +46,15 @@ public class MainActivity extends AppCompatActivity {
         startActivityForResult(intencja, 1);
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 1 && requestCode == RESULT_OK)
+        {
+            Bundle extras= data.getExtras();
+            String nowy = (String) extras.get("wpis");
+            target.add(nowy);
+            adapter.notifyDataSetChanged();
+        }
+    }
+
 }
