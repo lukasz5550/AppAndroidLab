0b8092eb3f7d9d608e007345d4f907e32ba7a4c2
lukasz5550 <lukasz5550@poczta.onet.pl>
2018-04-07 16:16:43 +0200

    Commit3

--- app/src/main/java/com/ldmitrowski/wizut/appandroidlab/MainActivity.java
+++ app/src/main/java/com/ldmitrowski/wizut/appandroidlab/MainActivity.java
@@ -1,7 +1,11 @@
 package com.ldmitrowski.wizut.appandroidlab;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -28,4 +32,18 @@ public class MainActivity extends AppCompatActivity {
         ListView listview = (ListView) findViewById(R.id.listView);
         listview.setAdapter(this.adapter);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    public void nowyWpis(MenuItem mi){
+        Intent intencja = new Intent(this , DodajWpis.class);
+        startActivityForResult(intencja, 1);
+    }
+
 }
